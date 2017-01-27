470cf3969160d24b54eaaefc2cdd79ddbb39aa1e
wbrzosko <wbrzosko@gmail.com>
2017-01-27 09:48:48 +0100

    Content Update
    Added parallax, some custom js and the new content.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'angular-parallax']);
 
 myApp.config(function ($routeProvider) {
     $routeProvider
--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 angular.element(document).bind('scroll', function() {
-   if (document.body.scrollTop < 200) {
-       angular.element(document.querySelector('.navbar')).addClass('navbar-top'); 
+   if (document.body.scrollTop > 200) {
+       angular.element(document.querySelector('.navbar')).addClass('navbar-shrink'); 
     } else {
-        angular.element(document.querySelector('.navbar')).removeClass('navbar-top'); 
+        angular.element(document.querySelector('.navbar')).removeClass('navbar-shrink'); 
     }
 });
